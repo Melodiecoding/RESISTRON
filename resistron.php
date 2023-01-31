889078aa69174dd7669cbd8c98f44ace18a15503
clovis leconte <leconte.clovis97@gmail.com>
2023-01-31 16:13:53 +0100

    style

--- resistron.php
+++ resistron.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resistron.css">
     <title>Resistron</title>
 </head>
 <body>
@@ -11,6 +12,15 @@
 <h1>Resistron</h1>
 <h2>CALCULATE RESISTANCE<h2>
 </header>
+<div class="container">
+    <div class="resistance">
+        <div class="firstColor"></div>
+        <div class ="2Color"></div>
+        <div class ="3Color"></div>
+        <div class ="lastColor"></div>
+
+    </div>
+</div>
 <?php
 
 
