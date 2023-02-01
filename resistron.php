6618c4e624fbbd67686da809f5c3ed00614760ec
melodieot <melodie.olory@gmail.com>
2023-02-01 09:00:40 +0100

    Ajout de contenu et css

---
+++ resistron.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resistron.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Resistron</h1>
+    </header>
+    <div class="container">
+        <h2>CALCULATE RESISTANCE<h2>
+            <div class="resistance">
+                <div class="firstColor"></div>
+                <div class ="2Color"></div>
+                <div class ="3Color"></div>
+                <div class ="lastColor"></div>                   
+            </div>
+        </div>
+        <?php
+                    
+                    
+        ?>
+</body>
+</html>
