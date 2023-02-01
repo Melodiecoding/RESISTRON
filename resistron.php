45c79107b2fc2bed34f747d434ae9e44a3a4510c
clovis leconte <leconte.clovis97@gmail.com>
2023-02-01 10:41:04 +0100

    style

--- resistron.php
+++ resistron.php
@@ -13,7 +13,7 @@
 <h2>CALCULATE RESISTANCE</h2>
 </header>
 <div class="firstContainer">
-<div class="container"></div>
+<div class="ligne"></div>
     <div class="resistance">
         <div class="firstColor"></div>
         <div class ="segondColor"></div>
@@ -21,8 +21,77 @@
         <div class ="lastColor"></div>
 
     </div>
-    <div class="container"></div>
+    <div class="ligne"></div>
 </div>
+
+<div class ="chooseColor"> 
+<label for="pet-select">Choose a color resistance:</label>
+
+<select name="firstColor" id="firstColor-select">
+    <option value="">--Please choose a firstColor--</option>
+    <option value="black">Black</option>
+    <option value="brown">Brown</option>
+    <option value="red">Red</option>
+    <option value="orange">Orange</option>
+    <option value="yellow">Yellow</option>
+    <option value="green">Green</option>
+    <option value="Blue">Blue</option>
+    <option value="Purple">Purple</option>
+    <option value="Greys">Greys</option>
+    <option value="White">White</option>
+    <option value="Gold">Gold</option>
+    <option value="Silver">Silver</option>
+</select>
+
+<select name="segondColor" id="segondColor-select">
+    <option value="">--Please choose a segondColor--</option>
+    <option value="black">Black</option>
+    <option value="brown">Brown</option>
+    <option value="red">Red</option>
+    <option value="orange">Orange</option>
+    <option value="yellow">Yellow</option>
+    <option value="green">Green</option>
+    <option value="Blue">Blue</option>
+    <option value="Purple">Purple</option>
+    <option value="Greys">Greys</option>
+    <option value="White">White</option>
+    <option value="Gold">Gold</option>
+    <option value="Silver">Silver</option>
+</select>
+
+<select name="treeColor" id="treeColor-select">
+    <option value="">--Please choose a treeColor--</option>
+    <option value="black">Black</option>
+    <option value="brown">Brown</option>
+    <option value="red">Red</option>
+    <option value="orange">Orange</option>
+    <option value="yellow">Yellow</option>
+    <option value="green">Green</option>
+    <option value="Blue">Blue</option>
+    <option value="Purple">Purple</option>
+    <option value="Greys">Greys</option>
+    <option value="White">White</option>
+    <option value="Gold">Gold</option>
+    <option value="Silver">Silver</option>
+</select>
+
+<select name="lastColor" id="lastColor-select">
+    <option value="">--Please choose a lastColor--</option>
+    <option value="black">Black</option>
+    <option value="brown">Brown</option>
+    <option value="red">Red</option>
+    <option value="orange">Orange</option>
+    <option value="yellow">Yellow</option>
+    <option value="green">Green</option>
+    <option value="Blue">Blue</option>
+    <option value="Purple">Purple</option>
+    <option value="Greys">Greys</option>
+    <option value="White">White</option>
+    <option value="Gold">Gold</option>
+    <option value="Silver">Silver</option>
+</select>
+</div>
+<h2>Result</h2>
 <?php
 
 
